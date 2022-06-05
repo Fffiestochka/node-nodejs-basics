32771b803a8cf844c1f25ffc6eedbe0911dad697
Fffiestochka <Fffiestochka@mail.ru>
2022-06-05 17:51:49 +0300

    feat: add initial constatns to create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs/promises');
+
 export const create = async () => {
-    // Write your code here 
+  try {
+    const pathToNewFile = path.join(__dirname, 'fresh.txt');
+    const content = 'I am fresh and young';
+    const isFileExist = await fsPromises.access(pathToNewFile);
+    console.log(isFileExist);
+
+    // await fsPromises.writeFile(pathToNewFile, content);
+  } catch {}
 };
+
