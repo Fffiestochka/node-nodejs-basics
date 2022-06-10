04b5a6b7f2607eb8297d9a7f7b53cfd59c11caee
Fffiestochka <Fffiestochka@mail.ru>
2022-06-10 21:41:39 +0300

    feat: task env.js done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
 export const parseEnv = () => {
-    // Write your code here 
+    const parsedVariables = Object.entries(process.env).reduce((variablesArr, [key, value]) => {
+        if (key.startsWith('RSS_')) variablesArr.push(`${key} = ${value}`);
+        return variablesArr;
+    }, []);
+
+    console.log(parsedVariables.join(' '));
+
 };
+
+parseEnv();
