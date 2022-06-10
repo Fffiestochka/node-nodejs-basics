866fc3171edbf57999e53a1e7d2b10e77e0f2b7c
Fffiestochka <Fffiestochka@mail.ru>
2022-06-10 18:55:26 +0300

    feat: create and copy functions implemented

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,15 +1,19 @@
-const fs = require('fs');
-const path = require('path');
-const fsPromises = require('fs/promises');
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { writeFile } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const create = async () => {
+  const src = __dirname + '/files/fresh.txt';
+  const existMsg = 'FS operation failed';
+  const content = 'I am fresh and young';
   try {
-    const pathToNewFile = path.join(__dirname, 'fresh.txt');
-    const content = 'I am fresh and young';
-    const isFileExist = await fsPromises.access(pathToNewFile);
-    console.log(isFileExist);
-
-    // await fsPromises.writeFile(pathToNewFile, content);
-  } catch {}
+    await writeFile(src, content, { flag: 'wx' });
+  } catch (error) {
+    throw new Error(existMsg);
+  }
 };
 
+create();
