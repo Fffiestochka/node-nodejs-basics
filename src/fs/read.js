1b13166c87f86dfdcd97373ae88948c0e1e7d2a8
Fffiestochka <Fffiestochka@mail.ru>
2022-06-10 21:08:09 +0300

    feat: implement func. read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,27 @@
+import { getPath } from './getPath.js';
+import { readFile } from 'fs/promises';
+
 export const read = async () => {
-    // Write your code here 
+  try {
+    const pathToFile = getPath(import.meta.url, 'fileToRead.txt');
+    const fileContent = await readFile(pathToFile, 'utf-8');
+    console.log(fileContent);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
+
+// export const read = async () => {
+//   try {
+//     const pathToFile = getPath(import.meta.url, 'fileToRead.txt');
+//     console.log(pathToFile);
+//     const fileContent = createReadStream(pathToFile, 'utf-8');
+//     let data = '';
+//     fileContent.on('data', (chunk) => (data += chunk));
+//     fileContent.on('end', () => console.log(data));
+//   } catch (err) {
+//     throw new Error('FS operation failed');
+//   }
+// };
+
+read();
