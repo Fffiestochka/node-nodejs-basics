e132c29366be3969b876b23a59a5a870afd40d08
Fffiestochka <Fffiestochka@mail.ru>
2022-06-11 13:00:33 +0300

    feat: implements func. compress, decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,18 @@
+import {createGzip} from 'zlib';
+import {createReadStream, createWriteStream} from 'fs';
+import {pipeline} from 'stream';
+import { getPath } from '../fs/getPath.js';
+
 export const compress = async () => {
-    // Write your code here 
+     const pathToInitFile = getPath(import.meta.url, 'fileToCompress.txt');
+     const pathToFinalFile = getPath(import.meta.url, 'archive.gz');
+     const initialContent = createReadStream(pathToInitFile);
+     const compressedContent = createWriteStream(pathToFinalFile);
+     const gzip = createGzip();
+
+     pipeline(initialContent, gzip, compressedContent, (err) => {
+       if (err) throw err;
+     });
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,19 @@
+import { createUnzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import { getPath } from '../fs/getPath.js';
+
 export const decompress = async () => {
-    // Write your code here 
+  const pathToCompressedFile = getPath(import.meta.url, 'archive.gz');
+  const pathToFinalFile = getPath(import.meta.url, 'fileToCompress2.txt');
+  const compressedContent = createReadStream(pathToCompressedFile);
+  const finalContent = createWriteStream(pathToFinalFile);
+
+  const unzip = createUnzip();
+
+  pipeline(compressedContent, unzip, finalContent, (err) => {
+    if (err) throw err;
+  });
 };
+
+decompress();
